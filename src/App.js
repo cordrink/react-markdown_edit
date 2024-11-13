c845b47a4e5b9ee0f241707fcc7dd8e50ca8f369
cordrink <masodavictorjudicael@gmail.com>
2024-11-13 20:58:43 +0100

    Realisation d'un editeur de markdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {sampleText} from "./sampleText";
+import {useEffect, useState} from "react";
+import {marked} from "marked";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [sampleTxt, setSampleTxt] = useState(() => {
+        const savedTxt = localStorage.getItem("sampleTxt");
+        return savedTxt ? savedTxt : sampleText;
+    });
+
+    useEffect(() => {
+        // met a jour le localstorage lorsque sampleTxt change avec une condition evitant un contenu vide
+        if (sampleTxt) {
+            localStorage.setItem("sampleTxt", sampleTxt);
+        }
+    }, [sampleTxt]);
+
+
+    const handleChange = (e) => {
+        const text = e.target.value;
+
+        setSampleTxt(text);
+    }
+
+    const textRender = () => {
+        let rawMarkup = marked.parse(sampleTxt);
+        return {__html: rawMarkup}
+    }
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6">
+                    <textarea className={"form-control"} rows="35" value={sampleTxt} onChange={handleChange}></textarea>
+                </div>
+                <div className="col-sm-6">
+                    <div dangerouslySetInnerHTML={textRender()}></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
